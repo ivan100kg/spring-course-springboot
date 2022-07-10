8fdf3bb2fc2e8f48196c836e36480afc9bb25984
Ivan B <ivar110kg@gmail.com>
2022-07-10 16:50:48 +0400

    Initial commit

---
+++ src/main/java/io/github/ivan100kg/springboot/springboot_rest/SpringbootRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.ivan100kg.springboot.springboot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApplication.class, args);
+	}
+
+}
